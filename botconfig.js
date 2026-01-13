319802d9b37b0fb5d27d04f97312a80be48555a9
rezamaulana2349-beep <rezamaulana2349@gmail.com>
2026-01-13 23:20:43 +0700

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,12 +1,12 @@
 module.exports = {
-  Admins: ["UserID", "UserID"], //DEV'S
+  Admins: ["1101826599386611792", "1101826599386611792"], //DEV'S
   ExpressServer: true, 
   DefaultPrefix: process.env.Prefix || ">",
   Port: 80, //Which port website gonna be hosted
   SupportServer: "https://discord.io/sciencegear", // Support Server Link
-  Token: process.env.Token || "", // Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", 
-  ClientSecret: process.env.Discord_ClientSecret || "", 
+  Token: process.env.Token || "MTQ2MDM2NDY3OTYwMzAzMjA5NQ.GXbpmX.dNY1dWs7pRfFs9R2Qqa5QQ2-R62YFBZtlciK2w", // Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "1460364679603032095", 
+  ClientSecret: process.env.Discord_ClientSecret || "oaQ8a76Om87M1p8nQwv1FZkmx8aJXLF0", 
   Scopes: ["identify", "guilds", "applications.commands"],
   ServerDeafen: true, 
   DefaultVolume: 100,
